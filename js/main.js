6011cd62418eb54ab852414153cc78d865a46f98
MakarCat <tsivikov.makar@gmail.com>
2022-03-28 20:32:39 +0300

    ver 0.15

--- js/main.js
+++ js/main.js
@@ -37,23 +37,46 @@ function close_animation(element, animationDown, animationUp) {
     });
 }
 
+
+var animationOpen = 'animate__fadeInLeft',
+    animationClose = 'animate__fadeOutLeft';
+
 function edit_book(event) {
     let book_id_str = event.currentTarget.parentElement.parentElement.id;
     const element = event.currentTarget.parentElement.parentElement.parentElement.children[2];
     let book_id = parseInt(book_id_str);
 
-    var animationDown = 'animate__slideOutUp',
-        animationUp = 'animate__slideInDown';
-
-    open_animation(element, animationUp);
+    open_animation(element, animationOpen);
 }
 
 function edit_book_accept(event) {
+    let book_id_str = event.currentTarget.parentElement.parentElement.parentElement.children[0].id;
+    const element = event.currentTarget.parentElement.parentElement;
+    let book_id = parseInt(book_id_str);
 
+    let img = element.querySelector('input[name="img"]').value;
+        book_name = element.querySelector('input[name="name"]').value;
+        author = element.querySelector('input[name="author"]').value;
+        rate = parseInt(element.querySelector('input[name="rate"]').value);
+        book_status = parseInt(element.querySelector('input[name="status"]').value);
+    
+    alert(book_id+" "+img+" "+book_name+" "+author+" "+rate+" "+book_status);
+
+    $.post(
+        "php/edit_book.php", {
+            id:book_id,
+            img:img,
+            name:book_name,
+            author:author,
+            rate:rate, 
+            status:book_status
+        }, edit_successfully
+    );
 }
 
 function edit_book_unaccept(event) {
-    
+    const element = event.currentTarget.parentElement.parentElement;
+    close_animation(element, animationClose, animationOpen);
 }
 
 function edit_successfully() {
---
+++ php/edit_book.php
@@ -0,0 +1,50 @@
+<?php
+    require_once "../config/config.php";
+
+    $id     = $_POST['id'];
+    $img    = $_POST['img'];
+    $name   = $_POST['name'];
+    $author = $_POST['author'];
+    $rate   = (int)$_POST['rate'];
+    $status = $_POST['status'];
+
+    $book = mysqli_query($connect, "SELECT * FROM `books` WHERE `ID`=$id");
+
+    $querySql = [];
+
+    echo "test";
+
+    if ($author) {
+        $querySql[] = "author = '{$author}'";
+    } 
+    if ($img) {
+        $querySql[] = "img = '{$img}'";
+    }
+    if ($name) {
+        $querySql[] = "name = '{$name}'";
+    }
+    if(!is_nan($rate) && $rate) {
+        $querySql[] = "rate = '{$rate}'";
+    }
+    $query = implode(",", $querySql);
+    /*
+    if (is_nan($img) or $img === none) $query = $query + "img='$book['img'], ";
+    else                               $query = $query + "img='$img', ";
+
+    if (is_nan($name) or $name === none) $query = $query + "name='$book['name'], ";
+    else                               $query = $query + "name='$name', ";
+
+    if (is_nan($author) or $author === none) $query = $query + "author='$book['author'], ";
+    else                               $query = $query + "author='$author', ";
+
+    if (is_nan($rate) or $rate === none) $query = $query + "rate='$book['rate'] ";
+    else                               $query = $query + "rate='$rate' ";
+    /*
+    if (is_nan($status) or $status === none) $query = $query + "status='$book['status'], ";
+    else                               $query = $query + "status='$status', ";
+    */
+
+    echo $query;
+    mysqli_query($connect, "UPDATE `books` SET {$query} WHERE `ID`= {$id}");
+
+    //header("../index.php");
